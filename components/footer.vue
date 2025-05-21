cbd385e54dba0852b6ea36842d5da80ae73282f0
Sarvarchik2 <sarvarikvarvarik@gmail.com>
2025-05-21 19:06:47 +0500

    bug fix

--- components/footer.vue
+++ components/footer.vue
@@ -5,33 +5,28 @@
         <div class="footer-first">
           <img class="footer-logo" :src="FooterLogo" alt="logo" />
           <div class="footer-first-link">
-            <NuxtLink><img :src="Icon1" alt="link" /></NuxtLink>
-            <NuxtLink><img :src="Icon2" alt="link" /></NuxtLink>
-            <NuxtLink><img :src="Icon3" alt="link" /></NuxtLink>
-            <NuxtLink><img :src="Icon4" alt="link" /></NuxtLink>
+            <NuxtLink to="https://www.facebook.com/share/15egaUiuFb/?mibextid=wwXIfr"><img :src="Icon1" alt="link" /></NuxtLink>
+            <NuxtLink to="https://www.instagram.com/_yas_auto_/profilecard/?igsh=bGt5emFiY2QxaHNy"><img :src="Icon2" alt="link" /></NuxtLink>
+            <NuxtLink to="https://t.me/yas_auto"><img :src="Icon4" alt="link" /></NuxtLink>
           </div>
         </div>
 
         <ul class="footer-list">
           <h2>{{ $t('footer.cars') }}</h2>
           <li><NuxtLink to="/models">Li 9</NuxtLink></li>
-          <li><NuxtLink to="/models">Li 8</NuxtLink></li>
           <li><NuxtLink to="/models">Li 7</NuxtLink></li>
           <li><NuxtLink to="/models">Li 6</NuxtLink></li>
         </ul>
 
         <ul class="footer-list">
           <h2>{{ $t('footer.company') }}</h2>
-          <li><NuxtLink to="/contact">{{ $t('footer.magazine') }}</NuxtLink></li>
           <li><NuxtLink to="/contact">{{ $t('footer.contacts') }}</NuxtLink></li>
           <li><NuxtLink to="/#about-us">{{ $t('footer.about') }}</NuxtLink></li>
         </ul>
 
         <ul class="footer-list">
           <h2>{{ $t('footer.purchase') }}</h2>
-          <li><NuxtLink to="/contact">{{ $t('footer.find_dealer') }}</NuxtLink></li>
           <li><NuxtLink to="/contact">{{ $t('footer.test_drive') }}</NuxtLink></li>
-          <li><NuxtLink to="/contact">{{ $t('footer.leasing') }}</NuxtLink></li>
           <li><NuxtLink to="/contact">{{ $t('footer.offer') }}</NuxtLink></li>
         </ul>
 
@@ -49,10 +44,9 @@
 
 <script setup lang="ts">
 import FooterLogo from '@/assets/footer/Sub Container.png'
-import Icon1 from '@/assets/footer/Button.png'
-import Icon2 from '@/assets/footer/Button (1).png'
-import Icon3 from '@/assets/footer/Button (2).png'
-import Icon4 from '@/assets/footer/Button (3).png'
+import Icon1 from '@/assets/footer/Button (1).png'
+import Icon2 from '@/assets/footer/inst.png'
+import Icon4 from '@/assets/footer/telegram.png'
 </script>
 <style>
 @import "./footer.css";
